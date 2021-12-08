49cf035e1bc3fc89ad3ac4cfece3dd86898d9423
ausprouty <bob.prouty@powertochange.org.au>
2021-12-08 15:25:26 +1100

    added MC2

---
+++ public/sites/mc2/php/cmn/UsbDirectory.php
@@ -0,0 +1,32 @@
+<?php
+
+    $dir = "M:/MC2/USB2020";
+    //header('Content-type: text/html; charset=utf-8');
+    //echo nl2br("starting UBB\n");
+    // listFolderFiles($dir);
+    header('Content-type: text/html; charset=utf-8');
+    $files = scandir($dir);
+    echo "<pre>";
+    print_r($files);
+    echo "</pre>";
+
+
+
+function listFolderFiles($dir){
+    $ffs = scandir($dir);
+
+    unset($ffs[array_search('.', $ffs, true)]);
+    unset($ffs[array_search('..', $ffs, true)]);
+
+    // prevent empty ordered elements
+    if (count($ffs) < 1)
+        return;
+
+    echo '<ol>';
+    foreach($ffs as $ff){
+        echo '<li>'.$ff;
+        if(is_dir($dir.'/'.$ff)) listFolderFiles($dir.'/'.$ff);
+        echo '</li>';
+    }
+    echo '</ol>';
+}
