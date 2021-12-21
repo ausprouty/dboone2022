7489180b9dccdd0001fb3876f95302bb676d3b11
ausprouty <bob.prouty@powertochange.org.au>
2021-12-21 17:05:33 +1100

    Removed old prototype files

--- public/sites/default/php/publishSeriesAndChapters.php
+++ public/sites/default/php/publishSeriesAndChapters.php
@@ -1,43 +1,44 @@
 <?php
-
+myRequireOnce ('create.php');
 myRequireOnce ('publishFiles.php');
 myRequireOnce ('publishSeries.php');
 myRequireOnce ('publishPage.php');
-myRequireOnce ('create.php');
+myRequireOnce ('writeLog.php');
+
 
 function publishSeriesAndChapters ($p){
-    $p['status'] = 'publish';
+
     // first prototype the Series Index
     $out = publishSeries ($p);
     $files_json = $out['files_json']; // this starts file for download of series
     $files_in_pages = [];
-    //$out['debug'] .= 'In prototypeSeriesAndChapters '. "\n";
-   
+    //$debug .= 'In prototypeSeriesAndChapters '. "\n";
+
     // find the list of chapters that are ready to publish
     $series = objectFromRecnum($p['recnum']);
     $series_dir = ROOT_PUBLISH_CONTENT .  $series->country_code . '/'. $series->language_iso . '/'. $series->folder_name . '/';
     // make sure folder exists
     if (!file_exists( $series_dir)){
-        dirMake ($series_dir); 
+        dirMake ($series_dir);
     }
     $text = json_decode($series->text);
     $chapters = $text->chapters;
     foreach ($chapters as $chapter){
         if ($chapter->publish){
-            $sql = "SELECT recnum FROM  content 
-                WHERE  country_code = '" . $series->country_code ."' 
-                AND language_iso = '" . $series->language_iso ."' 
-                AND folder_name = '" . $series->folder_name ."' 
-                AND filename = '" . $chapter->filename ."' 
+            $sql = "SELECT recnum FROM  content
+                WHERE  country_code = '" . $series->country_code ."'
+                AND language_iso = '" . $series->language_iso ."'
+                AND folder_name = '" . $series->folder_name ."'
+                AND filename = '" . $chapter->filename ."'
                 AND prototype_date IS NOT NULL
                 ORDER BY recnum DESC LIMIT 1";
-            //$p['debug'] .= $sql. "\n";
+            //$debug .= $sql. "\n";
             $data = sqlArray($sql);
             if ($data){
                 $p['recnum'] = $data['recnum'];
                 // need to find latest record for recnum
                 $result =  publishPage ($p);
-                $out['debug'] .= $result['debug'];
+                $debug .= $result['debug'];
                 if (is_array($result)){
                     $files_in_pages = array_merge($files_in_pages,$result['files_in_page']);
                 }
@@ -45,7 +46,7 @@ function publishSeriesAndChapters ($p){
             else{
                 // find file and add to database
                 $file =  ROOT_EDIT. $series_dir .  $chapter->filename . '.html';
-               // $out['debug'] .= 'looking for ' . $file . "\n";
+               // $debug .= 'looking for ' . $file . "\n";
                 if (file_exists($file)){
                     $p['text'] = file_get_contents($file);
                     $p['filename'] = $chapter->filename;
@@ -56,13 +57,13 @@ function publishSeriesAndChapters ($p){
                     if (is_array($result['files_in_page'])){
                         $files_in_pages = array_merge($files_in_pages,$result['files_in_page']);
                     }
-                    //$out['debug'] .= $result['debug'];
+                    //$debug .= $result['debug'];
                 }
                 else{
-                    $out['debug'] .= 'NO RESULT for ' . $file. "\n";
+                    $debug .= 'NO RESULT for ' . $file. "\n";
                 }
             }
-           
+
         }
     }
 
@@ -72,10 +73,10 @@ function publishSeriesAndChapters ($p){
     foreach ($files_in_pages as $json){
         $files_json.= '{"url":"'. $json .'"},' ."\n";
     }
-    $files_json = substr($files_json, 0, -2) . "\n" . ']' . "\n" ; 
+    $files_json = substr($files_json, 0, -2) . "\n" . ']' . "\n" ;
     $fh = fopen( $series_dir . 'files.json', 'w');
     fwrite($fh, $files_json);
-    fclose($fh);     
-    $out['data'] = 'Finished';
-    return $out;
+    fclose($fh);
+    writeLog('publishSeriesAndChapters', $debug);
+    return true;
 }
